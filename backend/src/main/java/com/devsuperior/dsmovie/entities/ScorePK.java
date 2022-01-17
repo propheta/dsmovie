a91ff2edc1d8571b8654b3ccb3d41bd5f93ce86d
Gabriel Bolea <gabriel.a.bolea@gmail.com>
2022-01-17 11:33:38 +0000

    Save score

--- backend/src/main/java/com/devsuperior/dsmovie/entities/ScorePK.java
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/ScorePK.java
@@ -17,6 +17,7 @@ public class ScorePK implements Serializable {
 	@ManyToOne
 	@JoinColumn(name = "movie_id")
 	private Movie movie;
+	
 	@ManyToOne
 	@JoinColumn(name = "user_id")
 	private User user;
